66eee283285e7525969ec7dc78e25dd9e920a54f
RitanMihai <lucian.ritan@gmail.com>
2022-10-15 15:52:39 +0300

    Hello World example

---
+++ src/main/java/com/example/servletexample/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.example.servletexample;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
